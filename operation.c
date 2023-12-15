9db49b1fb04879e05be06e77d87e3997e41b86d6
AlCo2 <abdou.lahboub.wwe@gmail.com>
2023-12-15 02:35:44 +0100

    new task mod

--- operation.c
+++ operation.c
@@ -44,3 +44,27 @@ void mul(stack_t **stack, unsigned int line)
 	*stack = temp;
 }
 
+/**
+ * mod - function to mod two element in stack
+ *
+ * @stack: the head
+ * @line: the line number
+*/
+void mod(stack_t **stack, unsigned int line)
+{
+	int mod;
+	stack_t *temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+		handle_error2(12, line);
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line);
+		exit(EXIT_FAILURE);
+	}
+	mod = (*stack)->next->n % (*stack)->n;
+	temp = (*stack)->next;
+	temp->n = mod;
+	free(*stack);
+	*stack = temp;
+}
